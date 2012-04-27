e4f7ad069219ca32e162b512a0c77dfd0fdb6c17
Graham Neubig <neubig@gmail.com>
2012-04-27 09:08:14 +0900

    Added changes

--- src/bin/nafil.cc
+++ src/bin/nafil.cc
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <nafil/config-runner.h>
+#include <nafil/nafil-runner.h>
 
+using namespace nafil;
 using namespace std;
 
-int main() {
-    cout << "Hello world!" << endl;
+int main(int argc, char** argv) {
+    // load the arguments
+    ConfigRunner conf;
+    vector<string> args = conf.loadConfig(argc,argv);
+    // train the reorderer
+    NafilRunner runner;
+    runner.Run(conf);
 }
