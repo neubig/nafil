6f8ccd789a7a89c119b8bbf0670f0d2964712ef3
Graham Neubig <neubig@gmail.com>
2012-06-13 13:29:19 +0900

    Added stuff

--- script/filter-sentences.pl
+++ script/filter-sentences.pl
@@ -9,21 +9,22 @@ binmode STDIN, ":utf8";
 binmode STDOUT, ":utf8";
 binmode STDERR, ":utf8";
 
-if(@ARGV == 5) {
-    print STDERR "Usage: $0 INF INE ANSWER OUTF OUTE\n";
+if((@ARGV != 5) and (@ARGV != 6)) {
+    print STDERR "Usage: $0 INF INE ANSWER OUTF OUTE [CORRECT]\n";
     exit 1;
 }
 
 open INF, "<:utf8", $ARGV[0] or die "Could not open $ARGV[0]";
 open INE, "<:utf8", $ARGV[1] or die "Could not open $ARGV[1]";
-open ANSWER, "<:utf8", $ARGV[2] or die "Could not open $ARGV[2]";
+open INA, "<:utf8", $ARGV[2] or die "Could not open $ARGV[2]";
 open OUTF, ">:utf8", $ARGV[3] or die "Could not open $ARGV[3]";
 open OUTE, ">:utf8", $ARGV[4] or die "Could not open $ARGV[4]";
+my $CORRECT = ($ARGV[5] ? $ARGV[5] : 1);
 
 my ($f, $e, $a);
 while(($f = <INF>) and ($e = <INE>) and ($a = <INA>)) {
     chomp $a;
-    if($a == 1) {
+    if($a == $CORRECT) {
         print OUTF $f;
         print OUTE $e;
     }
